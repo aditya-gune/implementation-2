0b90cfc8cb2df9af5f412dc39b9f4bf553f0a5ac
agune001 <ca_adi@yahoo.com>
2016-10-15 14:30:32 -0700

    read training data in

--- testfile_AG.m
+++ testfile_AG.m
@@ -1,1 +1,31 @@
-%implementation assignmentt 2
+%implementation assignment 2
+dir = 'D:\Aditya\Desktop\School\OSU\MS\Term 1\CS534 - Machine Learning\Implementation 2';
+%'D:\Aditya\Desktop\School\OSU\MS\Term 1\CS534 - Machine Learning\Implementation 2\clintontrump.bagofwords.train'
+%'D:\Aditya\Desktop\School\OSU\MS\Term 1\CS534 - Machine Learning\Implementation 2\clintontrump.labels.train'
+
+
+%make dynamic later
+n=6000;
+
+i = 0;
+
+%open training file, read data in
+tfile = fopen('D:\Aditya\Desktop\School\OSU\MS\Term 1\CS534 - Machine Learning\Implementation 2\clintontrump.bagofwords.train');
+lfile = fopen('D:\Aditya\Desktop\School\OSU\MS\Term 1\CS534 - Machine Learning\Implementation 2\clintontrump.labels.train');
+tline = fgetl(tfile);
+lline = fgetl(lfile);
+tData = cell(0,1);
+lData = cell(0,1);
+while ischar(tline)
+    tData{end+1,1} = tline;
+    lData{end+1,1} = lline;
+    tline = fgetl(tfile);
+    lline = fgetl(lfile);
+end
+tData(:,2) = lData;
+fclose(tfile);
+fclose(lfile);
+
+
+
+
