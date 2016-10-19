af4c22e930f2da6241ac6d61ea55c90e4c08170b
agune001 <ca_adi@yahoo.com>
2016-10-18 19:48:05 -0700

    got wordcounts actually working

--- testfile_AG.m
+++ testfile_AG.m
@@ -44,24 +44,31 @@ while ~feof(fid)
     dictionary(end+1,1:2) = cat(1,tline{:});
 end
 
-
-[wc, wc_H,wc_D] = trainMultinomial(tData, dictionary);
+[wc, twc_H, twc_D] = trainBernoulli(tData, dictionary);
+[wc, wc_H, wc_D, log_pdist_H, log_pdist_D] = trainMultinomial(tData, dictionary);
 %BERNOULLI IMPLEMENTATION
-function wordcount = trainBernoulli(tData, dictionary)
+function [wordcount, tweetCount_H, tweetCount_D] = trainBernoulli(tData, dictionary)
     wordcount = zeros(length(dictionary), 2);
+    tweetCount_H = 0;
+    tweetCount_D = 0;
     for j = 1:length(tData) 
         disp('-------------');
         disp(j)
         disp(tData(j,2))
         words = tData{j};
+        if strcmp(tData(j,2), 'HillaryClinton')
+            tweetCount_H = tweetCount_H + 1;
+        else
+            tweetCount_D = tweetCount_D + 1;
+        end
         for z=1:size(words,2) %iterate thru matrix
             if strcmp(tData(j,2), 'HillaryClinton')
                 if int32(words{z}) > 0
-                    wordcount(int32(words{z}),1) = 1;
+                    wordcount(int32(words{z}),1) = wordcount(int32(words{z}),1) + 1;
                 end
             else
                 if int32(words{z}) > 0
-                    wordcount(int32(words{z}),2) = 1;
+                    wordcount(int32(words{z}),2) = wordcount(int32(words{z}),2) + 1;
                 end
             end
         end %end of this tweet (as a matrix)
@@ -70,16 +77,18 @@ function wordcount = trainBernoulli(tData, dictionary)
 end
 
 %MULTINOMIAL IMPLEMENTATION
-function [wordcount, wc_H, wc_D] = trainMultinomial(tData, dictionary)
-    wc_H = 0;
-    wc_D = 0;
+function [wordcount, wc_H, wc_D, log_pdist_H, log_pdist_D] = trainMultinomial(tData, dictionary)
+    wc_H = 0; %total # of words tweeted by Hillary
+    wc_D = 0; %total # of words tweeted by Trump
+    log_pdist_H = 0;
+    log_pdist_D = 0;
     wordcount = zeros(length(dictionary), 4);
     for j = 1:length(tData) 
         words = tData{j};
         if strcmp(tData(j,2), 'HillaryClinton')
-            wc_H = wc_H + size(tData(j));
+            wc_H = wc_H + size(tData{j},2);
         else
-            wc_D = wc_D + size(tData(j));
+            wc_D = wc_D + size(tData{j},2);
         end
         for z=1:size(words,2) %iterate thru matrix
             if strcmp(tData(j,2), 'HillaryClinton')
@@ -101,12 +110,15 @@ function [wordcount, wc_H, wc_D] = trainMultinomial(tData, dictionary)
         denom = (wc_H + length(wordcount));
         denom = denom(1);
         wordcount(x,3) = num/denom(1);
+        disp(log(wordcount(x,3)));
+        log_pdist_H = log_pdist_H + log(wordcount(x,3));
         
         %populate p(x_i|y=trump)
         num = wordcount(x,2) + 1; %laplace smoothing
         denom = (wc_D + length(wordcount));
         denom = denom(1);
         wordcount(x,4) = num/denom(1);
+        log_pdist_D = log_pdist_D + log(wordcount(x, 4));
         
     end
     
